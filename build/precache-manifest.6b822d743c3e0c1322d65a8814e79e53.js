5d26f438f121d9405514be3ef872c2eb7a9c843b
t4christ <bakaretemitayo7@gmail.com>
2019-04-03 09:39:52 +0100

    made modifications to package.json

---
+++ build/precache-manifest.6b822d743c3e0c1322d65a8814e79e53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69744f9b6a8be2705411",
+    "url": "/bluebic/static/js/runtime~main.9aeec41c.js"
+  },
+  {
+    "revision": "00e1121a0c61350ca3f6",
+    "url": "/bluebic/static/js/main.f81e69f6.chunk.js"
+  },
+  {
+    "revision": "b36392922cdba90d24b7",
+    "url": "/bluebic/static/js/2.cc27f640.chunk.js"
+  },
+  {
+    "revision": "00e1121a0c61350ca3f6",
+    "url": "/bluebic/static/css/main.5debecfb.chunk.css"
+  },
+  {
+    "revision": "014988e721a9d9ee13f8ca3ba9d0fc10",
+    "url": "/bluebic/index.html"
+  }
+];
